f1f9e2b10de72928fc72bad7c11ab157fe00cef9
jeholtre <jeholtre@mtu.edu>
2021-03-21 20:26:15 -0400

    cheeky 404 solution?

--- src/routes.js
+++ src/routes.js
@@ -10,19 +10,17 @@ import About from './Components/About';
 
 const Main = () => {
     return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-        <Switch>
-            <Route exact path='/' render={() => {
-                return <Home/>
-            }}/>
-            <Route exact path='/Login' render={() =>{
-                return <Login/>
-            }}/>
-            <Route exact path='/About' render={() =>{
-                return <About/>
-            }}/>
-        </Switch>
-    </HashRouter>
+    <Switch>
+        <Route exact path='/' render={() => {
+            return <Home/>
+        }}/>
+        <Route exact path='/Login' render={() =>{
+            return <Login/>
+        }}/>
+        <Route exact path='/About' render={() =>{
+            return <About/>
+        }}/>
+    </Switch>
     );
 };
 
