932731e3e16f68eeff005bc452307681de28b53e
ljjohnst <55628783+ljjohnst@users.noreply.github.com>
2021-03-28 13:10:04 -0400

    About page Styling, Back and Help buttons, Help Modal

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,37 +1,77 @@
 import React from 'react';
-import { Header, Divider } from 'semantic-ui-react';
+import {Header, Divider, Segment, Button, Popup, Modal} from 'semantic-ui-react';
 import '../App.css';
-function About()
-{
-    return (
-        <div className={'About'}>
-            <div className={'App-header'}>
-            <Header size={'large'} >About Page</Header>
-                <p>this is the about page</p>
-            <Divider hidden />
-            <Header size={'large'}>Project Description</Header>
-                <p>Wet-Dry-Mapping is an app designed to help with the data collection process
-                    associated with mapping streams. This product allows you to take GPS data points
-                    while walking. During the data collection process you can say whether the stream
-                    is Wet - Neither - Dry and if it is Wet then whether it is a Pool (Slow moving or no movement)
-                    or Riffle (Fast moving)
-                </p>
+function About() {
+    const [open, setOpen] = React.useState(false)
 
-            <Divider hidden />
-            <Header size={'large'}>Frequently Asked Questions</Header>
-                <p>FAQ:</p>
-                <p>Q1:" Temp "</p>
-                <p>A1:" Temp "</p>
+            return (
+                <div className={"POI"}>
+                    <Header size={'large'} color={'green'}>About Page</Header>
+                    <Segment placeholder className={"placeHolder"}>
+                        <p>this is the about page, Temp, can add description here or delete section</p>
+                    </Segment>
+                    <Divider hidden/>
 
-            <Divider hidden />
-            <Header size={'large'}>Credits</Header>
-                <p>Streamers App Development Team:</p>
-                <p>Product Owner: Zong Deng</p>
-                <p>Project Technical Lead: Emma Ryden</p>
-                <p>Product Technicians: Jason Holtry, Luke Johnston, Mike Boisvert</p>
+                    <Header size={'large'} color={'green'}>Project Description</Header>
+                    <Segment placeholder className={"placeHolder"}>
+                        <p>Wet-Dry-Mapping is an app designed to help with the data collection process
+                            associated with mapping streams. This product allows you to take GPS data points
+                            while walking. During the data collection process you can say whether the stream
+                            is Wet - Neither - Dry and if it is Wet then whether it is a Pool (Slow moving or no
+                            movement)
+                            or Riffle (Fast moving)
+                        </p>
+                    </Segment>
+                    <Divider hidden/>
+                    <Header size={'large'} color={'green'}>Frequently Asked Questions</Header>
+                    <Segment placeholder className={"placeHolder"}>
+                        <p>FAQ:</p>
+                        <p>Q1:" Temp "</p>
+                        <p>A1:" Temp "</p>
+                    </Segment>
 
-            </div>
-        </div>
-    );
+                    <Divider hidden/>
+                    <Header size={'large'} color={'green'}>Credits</Header>
+                    <Segment placeholder className={"placeHolder"}>
+                        <p>Streamers App Development Team:</p>
+                        <p>Product Owner: Zong Deng</p>
+                        <p>Project Technical Lead: Emma Ryden</p>
+                        <p>Product Technicians: Jason Holtry, Luke Johnston, Mike Boisvert</p>
+                    </Segment>
+                    <p>
+                        <Button color={'green'} onClick={() => {
+                            window.location.href = "wet-dry#/"
+                        }}>Back</Button>
+                        <Popup
+                            trigger={<Button color={'green'}>Help (?)</Button>}
+                            content={'TESTING'}
+                            on={'click'}
+                        />
+
+                        <Modal
+                            onClose={() => setOpen(false)}
+                            onOpen={() => setOpen(true)}
+                            open={open}
+                            trigger={<Button color={'green'}>Modal Help</Button>}
+                        >
+                            <Modal.Header>Help for the About Page</Modal.Header>
+                            <Modal.Content image>
+                                <Modal.Description>
+                                    <p>
+                                        The About page offers basic information about the app and features some general FAQs
+                                    </p>
+                                    <p>If you need specific help on any page please press the help button near the bottom</p>
+                                </Modal.Description>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='green' onClick={() => setOpen(false)}>
+                                    Close Popup
+                                </Button>
+                            </Modal.Actions>
+                        </Modal>
+                    </p>
+                </div>
+            );
 }
+
 export default About;
