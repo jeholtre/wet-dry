61814ddb3e4d6939307462a2194a30c6c2aff43d
mdboisve <59846393+mdboisve@users.noreply.github.com>
2021-04-21 19:25:18 -0400

    Changes in confirmation page input fields are now loaded into local storage.

--- src/Components/DataCollectionConfirmation.js
+++ src/Components/DataCollectionConfirmation.js
@@ -14,10 +14,10 @@ function Confirmation()
     const [date, setDate] = useState(new Date());
     const [loading, setLoading] = useState(true);
     const [updateTime, setUpdateTime] = useState(1000);
-    const [username, setUserName] = useState('');
-    const [stream, setStream] = useState('');
-    const [streamSection, setStreamSection] = useState('');
-    const [sectionID, setSectionID] = useState('');
+    const [username, setUserName] = useState(localStorage.getItem('username'));
+    const [stream, setStream] = useState(localStorage.getItem('stream'));
+    const [streamSection, setStreamSection] = useState(localStorage.getItem('streamSection'));
+    const [sectionID, setSectionID] = useState(localStorage.getItem('sectionID'));
 
     const handleUsernameChange = (e, {value} ) => {
         setUserName(value)
@@ -83,34 +83,32 @@ function Confirmation()
                     <br></br>
                     <Segment>
                         <Form onSubmit={handleSubmit}>
+                            <Form.Field required>
                             <label>Name of Surveyor: </label>
                             <Form.Input
-                                required
                                 name='userName'
-                                value={localStorage.getItem('username')}
+                                value={username}
                                 onChange={handleUsernameChange}
                             />
                             <label>Stream: </label>
                             <Form.Input
-                                required
                                 name='stream'
-                                value={localStorage.getItem('stream')}
+                                value={stream}
                                 onChange={handleStreamChange}
                             />
                             <label>Stream Section: </label>
                             <Form.Input
-                                required
                                 name='streamSection'
-                                value={localStorage.getItem('streamSection')}
+                                value={streamSection}
                                 onChange={handleStreamSectionChange}
                             />
                             <label>Class/Section ID: </label>
                             <Form.Input
-                                required
                                 name='sectionID'
-                                value={localStorage.getItem('sectionID')}
+                                value={sectionID}
                                 onChange={handleSectionIDChange}
                             />
+                            </Form.Field>
                             <Button type="submit" color={'green'}  onClick={() => {
                                 setSubmitModal(true);
                                 clearLocalStorage();
